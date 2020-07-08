71c219dabde0d9cef4c9f6dc60eaa6a9dbc624d0
Black-x98 <tariq.hasan.csedu21@gmail.com>
2020-07-09 00:26:29 +0600

    commiting after some progress

--- Main.java
+++ Main.java
@@ -9,7 +9,7 @@ class BudgetManager{
     double total = 0;
     Scanner sc;
     double Balance;
-    ArrayList<String> ListOfPurchases = new ArrayList<String>();
+    ArrayList<String>[] ListOfPurchases = new ArrayList[4];
 
     void calculateTotal(){
 
@@ -26,8 +26,12 @@ class BudgetManager{
 
     void menu(){
 
+        for (int i=0; i<ListOfPurchases.length; i++) {
+            ListOfPurchases[i] = new ArrayList<>();
+        }
         sc = new Scanner(System.in);
         int choice = 999;
+        int NumOfPurchase = 0;
 
         while(true){
             System.out.println("Choose your action:\n" +
@@ -38,39 +42,132 @@ class BudgetManager{
                     "0) Exit");
 
             choice = sc.nextInt();
-            sc.nextLine();
+            String garbage = sc.nextLine();
             System.out.println();
 
-            if(choice==1){
+            if(choice==1){ // add income
                 System.out.println("Enter income:");
                 Balance += sc.nextDouble();
                 System.out.println("Income was added!");
             }
-            else if(choice==2){
-                System.out.println("Enter purchase name:");
-                String PurchaseName = sc.nextLine() + "";
-                System.out.println("Enter its price:");
-                double price = sc.nextDouble();
-                Balance -= price;
-                ListOfPurchases.add(PurchaseName + " $" + price);
-                System.out.println("Purchase was added!");
+            else if(choice==2){ // add purchase
+                while(true){
+                    System.out.println("\nChoose the type of purchases\n" +
+                            "1) Food\n" +
+                            "2) Clothes\n" +
+                            "3) Entertainment\n" +
+                            "4) Other\n" +
+                            "5) Back");
+                    int category = sc.nextInt();
+                    garbage = sc.nextLine();
+                    System.out.println();
+                    if(category==5){
+                        break;
+                    }
+                    else{
+                        System.out.println("Enter purchase name:");
+                        String PurchaseName = sc.nextLine() + "";
+                        System.out.println("Enter its price:");
+                        double price = sc.nextDouble();
+                        Balance -= price;
+                        ListOfPurchases[category-1].add(PurchaseName + " $" + price);
+                        System.out.println("Purchase was added!");
+                        NumOfPurchase += 1;
+                    }
+                }
             }
-            else if(choice==3){
-                if(ListOfPurchases.size()==0){
+            else if(choice==3){ // show purchase
+                int category = 0;
+                if(NumOfPurchase==0){
                     System.out.println("Purchase list is empty");
                 }
-                for(int i=0;i<ListOfPurchases.size();i++){
-                    System.out.println(ListOfPurchases.get(i));
+                else{
+                    while(category!=6){
+                        System.out.println("Choose the type of purchases\n" +
+                                "1) Food\n" +
+                                "2) Clothes\n" +
+                                "3) Entertainment\n" +
+                                "4) Other\n" +
+                                "5) All\n" +
+                                "6) Back");
+                        category = sc.nextInt();
+                        sc.nextLine();
+                        System.out.println();
+                        if(category==1){ // food
+                            if(ListOfPurchases[category-1].size()==0){
+                                System.out.println("Purchase list is empty");
+                            }
+                            else{
+                                System.out.println("Food:");
+                                for (int j = 0; j < ListOfPurchases[category-1].size(); j++) {
+                                    System.out.println(ListOfPurchases[category-1].get(j));
+                                }
+                                System.out.println();
+                            }
+                        }
+                        else if(category==2){ // clothes
+                            if(ListOfPurchases[category-1].size()==0){
+                                System.out.println("Purchase list is empty");
+                            }
+                            else{
+                                System.out.println("Clothes:");
+                                for (int j = 0; j < ListOfPurchases[category-1].size(); j++) {
+                                    System.out.println(ListOfPurchases[category-1].get(j));
+                                }
+                                System.out.println();
+                            }
+                        }
+                        else if(category==3){ // entertainment
+                            if(ListOfPurchases[category-1].size()==0){
+                                System.out.println("Purchase list is empty");
+                            }
+                            else{
+                                System.out.println("Entertainment:");
+                                for (int j = 0; j < ListOfPurchases[category-1].size(); j++) {
+                                    System.out.println(ListOfPurchases[category-1].get(j));
+                                }
+                                System.out.println();
+                            }
+                        }
+                        else if(category==4){ // other
+                            if(ListOfPurchases[category-1].size()==0){
+                                System.out.println("Purchase list is empty");
+                            }
+                            else{
+                                System.out.println("Other:");
+                                for (int j = 0; j < ListOfPurchases[category-1].size(); j++) {
+                                    System.out.println(ListOfPurchases[category-1].get(j));
+                                }
+                                System.out.println();
+                            }
+                        }
+                        else if(category==5){ // all
+                            if(ListOfPurchases[category-1].size()==0){
+                                System.out.println("Purchase list is empty");
+                            }
+                            else{
+                                System.out.println("All:");
+                                for(int i=0;i<ListOfPurchases.length;i++) {
+                                    for (int j = 0; j < ListOfPurchases[i].size(); j++) {
+                                        System.out.println(ListOfPurchases[i].get(j));
+                                    }
+                                }
+                                System.out.println();
+                            }
+                        }
+                    }
                 }
+
+
             }
-            else if(choice==4){
+            else if(choice==4){ // see balance
                 System.out.printf("Balance: $%.2f\n", Balance);
             }
-            else if(choice==0){
+            else if(choice==0){ // exit the program
                 System.out.println("Bye!");
                 break;
             }
-            else{
+            else{ // you made a boo-boo :v
                 System.out.println("Invalid choice");
             }
             System.out.println();
--- test.java
+++ test.java
@@ -1,12 +1,28 @@
-
+import java.util.ArrayList;
 
 public class test {
 
     public static void main(String[] args) {
-        String g = "djfbieubeib  uirfibo $54.55";
-        System.out.println(g);
-        String[] h = g.split("\\$");
-        System.out.println(h[1]);
+        ArrayList<String>[] L = new ArrayList[4];
+        for (int x = 0; x < L.length; x++) {
+            L[x] = new ArrayList<>();
+        }
+
+
+        L[0].add("ggg");
+        L[0].add("hhh");
+        L[1].add("ttt");
+        L[2].add("aaa");
+        L[3].add("bbb");
+
+        /*for(int i=0;i<L.length;i++){
+            for(int j=0;j<L[i].size();j++){
+                System.out.println(L[i].get(j));
+            }
+        }*/
+        for(int i=0;i<L.length;i++){
+            System.out.println(L[i]);
+        }
 
     }
 
